dc35c1de8e848d3b3b180964d8af909ec6e222ba
eloise.gomez <eloisegomez.etu@gmail.com>
2022-05-13 10:04:14 +0200

    add resource folder

---
+++ resource/get_projects.py
@@ -0,0 +1,13 @@
+import dataiku
+import dataikuapi
+
+def do(payload, config, plugin_config, inputs):
+    """
+    Create list of options of projects in instance.
+    """
+    remote_client = dataikuapi.DSSClient('https://se-global-demo-platform-ref.emea.dataiku-sandbox.io/', 'P0BRG0EVW8WXU3OELV53MG0QPDSV9S23')
+    projects = remote_client.list_projects()
+    choices = []
+    for project in projects:
+        choices.append({"value": project.get('projectKey'), "label": project.get('projectKey')})
+    return {"choices": choices}
